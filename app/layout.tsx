ef6390fb5b00ea97aebe3824c990e0f4393d8f28
Marin190 <marindelpech@gmail.com>
2025-07-11 14:35:55 +0200

    Google ads implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9078985664217000"></meta>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9078985664217000" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>{children}</body>
