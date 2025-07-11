2438322ffa94499a3be9856375e177bf84801534
Marin190 <marindelpech@gmail.com>
2025-07-11 14:08:25 +0200

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9078985664217000" crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
